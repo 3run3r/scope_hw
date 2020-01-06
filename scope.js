f468b3c31c5885aa0fa26ae4b9b64f8ae215536b
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-06 17:16:39 +0000

    finish ep 5

---
+++ scope.js
@@ -0,0 +1,85 @@
+// Episode 1
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Library',
+//   weapon: 'Rope'
+// };
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${scenario.murderer}.`;
+// }
+//
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// predicted output: logs "The murderer is 'Miss Scarlet'". We are accessing values that are not outside the scope.
+
+// Episode 2
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+
+// predicted output: error, const murderer can't be reassigned in the function changeMurderer.
+
+// Episode 3
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+
+// predicted output: "the murderer is Mrs. Peacock", the function is returning the variable that was reinitialised in the block. "the murderer is Professor Plum" the function is returning the original variable because it was only reinitialised in the scope of the first funciton.
+
+// // Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+//
+// const declareAllSuspects = function() {
+//   let suspectThree = 'Colonel Mustard';
+//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+//
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+//
+// // predicted output: "the suspects are Miss Scarlet Professor Plum Colonel Mustard", the suspectThree variable was reinitialised in the scope of the function. "Suspect three is Mrs. Peacock" because the variable was only reinitialisedin the scope of the function.
+
+// Episode 5
+// const scenario = {
+//   murderer: 'Miss Scarlet',
+//   room: 'Kitchen',
+//   weapon: 'Candle Stick'
+// };
+//
+// const changeWeapon = function(newWeapon) {
+//   scenario.weapon = newWeapon;
+// }
+//
+// const declareWeapon = function() {
+//   return `The weapon is the ${scenario.weapon}.`;
+// }
+//
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+
+// // predicted output: "the weapon is the revolver" the variable scenario is a const however its properties can be reassigned, which is what happens here.
